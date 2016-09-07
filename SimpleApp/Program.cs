73c7d0f68502f815ceb521af6ee03c5de3c4a075
kmpchk <kmpchk@gmail.com>
2016-09-07 22:16:00 +0400

    New Project Initial Add

---
+++ SimpleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SimpleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int f = 56;
+            string text = Console.ReadLine();
+            Console.WriteLine(text + "  " + "{0}", f);
+        }
+    }
+}
